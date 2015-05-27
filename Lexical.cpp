8455239df181ed3a66528901cc70dcaf6a9dd557
mark-stiles87 <mark-stiles87@users.noreply.github.com>
2015-05-27 15:39:49 -0400

    Update Lexical.cpp

--- Lexical.cpp
+++ Lexical.cpp
@@ -1,41 +1,13 @@
+/*
+Lexical Analysis
+System Architect:	Anthony Mauro
+Data Architect:		Benjamin Ciaglo
+Developer:			Mark Stiles
+Last Compiled 2015-05-27 on Dwarf_Box_72
+*/
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
 #include "Text.h"
-//int main()
-//{
-//string line;
-//string Token;
-//int count = 0;
-//ifstream inputfile("progsamp.dat");
-//if (inputfile.is_open())
-//{
-//while (getline(inputfile, line))
-//{
-//for (int i = 0; i < line.length(); i++)
-//{
-//if (line[i] != ' ')
-//{
-//int j = 0;
-//int k = i;
-//while (line[i] != ' ' && i < line.length())
-//{
-//j++;
-//i++;
-//}
-//Token.assign(line, k, j);
-//count++;
-//cout << count << " : " << Token << endl;
-//}
-//}
-//}
-//inputfile.close();
-//}
-//else
-//cout << "Error: Unable to open file.";
-//return 0;
-//}
 int main()
 {
 	Text test;
@@ -46,14 +18,18 @@ int main()
 	outfile.open("test.txt");
 	while (infile)
 	{
-		while (infile.peek() != ' '&&infile.peek() != '\t'&&infile.peek() != '\n'&&infile.peek() != '\0')
+		while (infile.peek() != ' ' && infile.peek() != '\t' && infile.peek() != '\n' && infile.peek() != '\0')
 			infile >> test;
 		count++;
 		outfile << count << ' ' << test << endl;
-		test.clear();
-		while (infile.peek() == ' '||infile.peek() == '\t'||infile.peek() == '\n'||infile.peek() == '\0')
+		if (infile.peek() == ' ')
 			infile >> test;
-		test.clear();
+		if (infile.peek() == '\t')
+			infile >> test;
+		if (infile.peek() == '\n')
+			infile >> test;
+		if (infile.peek() == '\0')
+			test.clear();
 	}
 	infile.close();
 	outfile.close();
