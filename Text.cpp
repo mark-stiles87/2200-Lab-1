67a9f204915844de50356e56020153a1db348b95
mark-stiles87 <mark-stiles87@users.noreply.github.com>
2015-05-27 18:47:32 -0400

    Update Text.cpp

--- Text.cpp
+++ Text.cpp
@@ -1,21 +1,21 @@
 #include "Text.h"
 Text::Text(const char*charSeq)
 {
-bufferSize = strlen(charSeq) + 1;
-buffer = new char[bufferSize];
-for (int i = 0; i < bufferSize; i++)
-	buffer[i] = charSeq[i];
+	bufferSize = strlen(charSeq) + 1;
+	buffer = new char[bufferSize];
+	for (int i = 0; i < bufferSize; i++)
+		buffer[i] = charSeq[i];
 }
 Text::Text(const Text& other)
 {
-bufferSize = other.bufferSize;
-buffer = new char[bufferSize];
-for (int i = 0; i < bufferSize; i++)
-	buffer[i] = other.buffer[i];
+	bufferSize = other.bufferSize;
+	buffer = new char[bufferSize];
+	for (int i = 0; i < bufferSize; i++)
+		buffer[i] = other.buffer[i];
 }
 void Text::operator= (const Text& other)
 {
-	if (!(*this==other))
+	if (!(*this == other))
 	{
 		delete[] buffer;
 		bufferSize = other.bufferSize;
@@ -23,68 +23,70 @@ void Text::operator= (const Text& other)
 		for (int i = 0; i < bufferSize; i++)
 			buffer[i] = other.buffer[i];
 	}
-return;
+	return;
 }
 Text::~Text()
 {
-delete[] buffer;
+	delete[] buffer;
 }
-int Text::getLength () const
+int Text::getLength() const
 {
-	return bufferSize-1;
+	return bufferSize - 1;
 }
 char Text::operator [] (int n) const
 {
-return buffer[n];
+	return buffer[n];
 }
 void Text::clear()
 {
-delete [] buffer;
-bufferSize = 1;
-buffer = new char[1];
-buffer[0] = '/0';
-return;
+	delete[] buffer;
+	bufferSize = 1;
+	buffer = new char[1];
+	buffer[0] = '/0';
+	return;
 }
-void Text::showStructure () const
+void Text::showStructure() const
 {
-int textLength = getLength();
-for (int i = 0; i<textLength; i++)
-cout << buffer[i];
-cout << endl;
-return;
+	int textLength = getLength();
+	for (int i = 0; i<textLength; i++)
+		cout << buffer[i];
+	cout << endl;
+	return;
+}
+Text Text::toUpper() const
+{
+	Text out(*this);
+	for (int i = 0; i < out.bufferSize; i++)
+		out.buffer[i] = static_cast<char>(toupper(out.buffer[i]));
+	return out;
+}
+Text Text::toLower() const
+{
+	Text out(*this);
+	for (int i = 0; i < out.bufferSize;i++)
+		out.buffer[i] = static_cast<char>(tolower(out.buffer[i]));
+	return out;
 }
 istream & operator >> (istream &input, Text &inputText)
-
 // Text input function. Extracts a string from istream input and
 // returns it in inputText. Returns the state of the input stream.
-
 {
-	const int textBufferSize = 256;     // Large (but finite)
-	char textBuffer[textBufferSize];   // text buffer
-
+	const int textBufferSize = 256; // Large (but finite)
+	char textBuffer[textBufferSize]; // text buffer
 	// Read a string into textBuffer, setw is used to prevent buffer
 	// overflow.
-
 	input >> setw(textBufferSize) >> textBuffer;
-
 	// Apply the Text(char*) constructor to convert textBuffer to
 	// a string. Assign the resulting string to inputText using the
 	// assignment operator.
-
 	inputText = textBuffer;
-
 	// Return the state of the input stream.
-
 	return input;
 }
-
 //--------------------------------------------------------------------
-
 ostream & operator << (ostream &output, const Text &outputText)
-
 // Text output function. Inserts outputText in ostream output.
 // Returns the state of the output stream.
-
 {
 	output << outputText.buffer;
 	return output;
